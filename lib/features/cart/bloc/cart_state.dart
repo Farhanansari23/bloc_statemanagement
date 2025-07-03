39269edd3c9b5b434de53d513a34199c09215e40
Farhan Ansari <drscorpion4@gmail.com>
2025-07-03 22:26:16 +0545

    FA-BS-05: working on cartPage

--- lib/features/cart/bloc/cart_state.dart
+++ lib/features/cart/bloc/cart_state.dart
@@ -1,6 +1,21 @@
+
+// Correct import (verify the path)
+import '../../home/models/home_product_model.dart';
+
+
 part of 'cart_bloc.dart';
 
 @immutable
 sealed class CartState {}
+abstract class CartActionableState extends CartState {}
 
 final class CartInitial extends CartState {}
+
+class CartSuccessState extends CartState{
+
+  final List<ProductsDataModel> cartItems;
+ CartSuccessState({required this.cartItems});
+}
+
+
+
--- lib/features/cart/ui/cart.dart
+++ lib/features/cart/ui/cart.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:bloc_statemanagement/features/cart/bloc/cart_bloc.dart';
+import 'package:bloc_statemanagement/features/cart/ui/cart_tile.dart';
+import 'package:bloc_statemanagement/features/home/models/home_product_model.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CartPage extends StatefulWidget {
   const CartPage({super.key});
@@ -8,28 +12,53 @@ class CartPage extends StatefulWidget {
 }
 
 class _CartPageState extends State<CartPage> {
+  final CartBloc cartBloc = CartBloc();
+
+  @override
+  void initState() {
+    cartBloc.add(CartInitialEvent());
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: _appBar(context),
-      body: _body(context),
-    );
+    return Scaffold(appBar: _appBar(context), body: _body(context));
   }
 
   PreferredSize _appBar(context) {
     return PreferredSize(
-        preferredSize: const Size.fromHeight(kToolbarHeight),
-        child: AppBar(
-          title: Text('Cart Page'),
-          centerTitle: true,
-          backgroundColor: Colors.teal,
-        ),
+      preferredSize: const Size.fromHeight(kToolbarHeight),
+      child: AppBar(
+        title: Text('Cart Page'),
+        centerTitle: true,
+        backgroundColor: Colors.teal,
+      ),
     );
   }
 
-  Widget _body(context){
-    return Container(
-      child: Text('Coming soon'),
+  Widget _body(context) {
+    return BlocConsumer<CartBloc, CartState>(
+      bloc: cartBloc,
+      listener: (context, state) {},
+      listenWhen: (previous, current) => current is CartActionableState,
+      buildWhen: (previous, current) => current is! CartActionableState,
+      builder: (context, state) {
+        switch (state.runtimeType) {
+          case CartSuccessState:
+            final successState = state as CartSuccessState;
+            return ListView.builder(
+              itemCount: successState.cartItems.length,
+              itemBuilder: (context, index) {
+                return CartTileWidget(
+                  productsDataModel: successState.cartItems[index],
+                  cartBloc: cartBloc,
+                );
+              },
+            );
+          default:
+        }
+        return Container();
+      },
     );
   }
 }
